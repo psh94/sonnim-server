045f2b725424b467b35f1b8a4882c3cd5b3a6f17
psh94 <suran5281@gmail.com>
2024-05-31 15:34:13 +0900

    first commit

---
+++ src/main/java/com/psh94/sonnim_server/SonnimServerApplication.java
@@ -0,0 +1,13 @@
+package com.psh94.sonnim_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonnimServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonnimServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psh94/sonnim_server/SonnimServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psh94.sonnim_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonnimServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
