3c1889ed99f5dc788b9bf78b0170b61b73693288
psh94 <suran5281@gmail.com>
2024-06-01 11:25:29 +0900

    final test

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-docker stop instagram-server || true
-docker rm instagram-server || true
+docker stop sonnim-server || true
+docker rm sonnim-server || true
 docker pull 058264214897.dkr.ecr.ap-northeast-2.amazonaws.com/sonnim-server:latest
-docker run -d --name instagram-server -p 8080:8080 058264214897.dkr.ecr.ap-northeast-2.amazonaws.com/sonnim-server:latest
+docker run -d --name sonnim-server -p 8080:8080 058264214897.dkr.ecr.ap-northeast-2.amazonaws.com/sonnim-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
